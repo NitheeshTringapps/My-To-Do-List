1a5ffed4f933a5e20c10122a1d988842c2aa0471
NitheeshTringapps <96728775+NitheeshTringapps@users.noreply.github.com>
2022-03-15 11:01:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+// Sign Up
+function storeCredentials(){
+    let name = document.getElementById("name").value;
+    let userName = document.getElementById("userName").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+    if(password === confirmPassword){
+        var details = {
+            "name": name,
+            "userName": userName,
+            "password": password
+        }
+        localStorage.setItem("details", JSON.stringify(details));
+    }
+    else{
+        alert("Password and Confirm Password is not same");
+        return false;
+    }
+}
+
+// Sign In
+function checkCredentials(){
+    let userName = document.getElementById("userName").value;
+    let password = document.getElementById("password").value;
+    var details = JSON.parse(localStorage.getItem("details"));
+    console.log(details.userName);
+    console.log(details.password);
+    if(userName == details.userName && password == details.password){
+        return true;
+    }
+    else{
+        alert("Invalid User Name or Password!");
+        return false;
+    }
+}
+
+// To Do List
 var myList = document.getElementById("myList");
 function addList(){
     var text = document.getElementById("newList").value;
@@ -25,4 +62,14 @@ function clearComplete(){
     for(var x of elements){
         x.remove();
     }
+}
+
+function save(){
+    var saveList = document.getElementById("myList").innerHTML;
+    localStorage.setItem("saveList", JSON.stringify(saveList));
+}
+
+function showSavedList(){
+    var savedList = localStorage.getItem("saveList");
+    document.getElementById("myList").innerHTML = JSON.parse(savedList);
 }
